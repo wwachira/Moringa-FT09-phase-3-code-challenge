aa94ba24278fcab5f55f45062d6c7a9a675ededa
lizwachira <lizwanguwachira@gmail.com>
2024-06-11 11:58:38 +0300

    db test

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,8 +13,9 @@ class TestModels(unittest.TestCase):
         self.assertEqual(article.title, "Test Title")
 
     def test_magazine_creation(self):
-        magazine = Magazine(1, "Tech Weekly")
+        magazine = Magazine(1, "Tech Weekly", "Technology")
         self.assertEqual(magazine.name, "Tech Weekly")
+        self.assertEqual(magazine.category, "Technology")
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     unittest.main()
